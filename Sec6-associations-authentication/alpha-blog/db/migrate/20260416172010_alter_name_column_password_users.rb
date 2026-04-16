6cf8c803e0aee8118f4bdacfb729709189f86748
Vinicius Bandeira <vinicius.bandeira.goncalves@gmail.com>
2026-04-16 14:30:12 -0300

    Configurando BCrypt

---
+++ Sec6-associations-authentication/alpha-blog/db/migrate/20260416172010_alter_name_column_password_users.rb
@@ -0,0 +1,5 @@
+class AlterNameColumnPasswordUsers < ActiveRecord::Migration[8.1]
+  def change
+    rename_column :users, :password, :password_digest
+  end
+end
--- Sec6-associations-authentication/alpha-blog/db/schema.rb
+++ Sec6-associations-authentication/alpha-blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_16_131039) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_16_172010) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at"
     t.text "description"
@@ -22,7 +22,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_16_131039) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "updated_at", null: false
     t.string "username"
   end
