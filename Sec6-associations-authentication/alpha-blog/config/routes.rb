c87714bfc4d5425e40ab90afa2f579796e381624
Vinicius Bandeira <vinicius.bandeira.goncalves@gmail.com>
2026-04-16 15:25:40 -0300

    Criando sign up de users

--- Sec6-associations-authentication/alpha-blog/config/routes.rb
+++ Sec6-associations-authentication/alpha-blog/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
   root "pages#home"
   get "about", to: "pages#about"
   # resources :articles, only: [:show] # desta forma, ele apenas utiliza a rota de show "excluindo" as demais
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] # desta forma, ele apenas utiliza a rota de show "excluindo" as demais
   resources :articles # desta forma, automaticamente o ruby ja entende que ele deve chamar as ações do crud, mas, além disso, ele tambem pode acessar todas as regra de um restfull
+  get "signup", to: "users#new"
+  # post "users", to: "users#create"
+  resources :users, excepet: [:new]
 end
